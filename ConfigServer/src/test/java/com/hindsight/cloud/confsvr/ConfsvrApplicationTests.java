d700d176fb3453a0d01db8730f7a6e15860abf1b
HanJaehee <hindsight@kakao.com>
2021-04-10 06:44:33 +0900

    Add ConfigServer

---
+++ ConfigServer/src/test/java/com/hindsight/cloud/confsvr/ConfsvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hindsight.cloud.confsvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfsvrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
